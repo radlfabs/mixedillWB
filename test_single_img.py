c20c49aff245a9ff0f97e7b79bfc9130a2de4a76
radlfabs <rosenthal.fabian@gmail.com>
2023-02-11 00:37:21 +0100

    clean up

--- test_single_img.py
+++ test_single_img.py
@@ -13,18 +13,14 @@ from tqdm import tqdm
 
 
 
-def single_test_net(net, device, img_dict, model_name, save_weights,
+def single_test_net(net, device, input_files, model_name, save_weights,
              multi_scale=False, keep_aspect_ratio=False, t_size=128,
              post_process=False, batch_size=32, wb_settings=None):
   """ Tests a trained network and saves the trained model in harddisk.
   """
   if wb_settings is None:
     wb_settings = ['D', 'S', 'T', 'F', 'C']
-  # input_files = dataset.Data.load_files(data_dir, mode='testing')
 
-  # if input_files == []:
-  #   input_files = dataset.Data.load_files(data_dir, mode='testing')
-  input_files = img_dict
   if multi_scale:
     test_set = single_img_dataset.Data(input_files, mode='testing', t_size=t_size,
                             wb_settings=wb_settings,
@@ -38,13 +34,14 @@ def single_test_net(net, device, img_dict, model_name, save_weights,
                             num_workers=0, pin_memory=True)
 
   
-  logging.info(f'''Starting testing:
+  logging.info(f"""
+        Starting testing:
         Model Name:            {model_name}
         Batch size:            {batch_size}
         WB settings:           {wb_settings}
         Save weights:          {save_weights}
         Device:                {device.type}
-  ''')
+  """)
 
 
   with torch.no_grad():
@@ -84,7 +81,6 @@ def single_test_net(net, device, img_dict, model_name, save_weights,
       c_img = c_img.to(device=device, dtype=torch.float32)
       imgs.append(c_img)
 
-      filename = batch['filename']
       weights = F.interpolate(
         weights, size=(d_img.shape[2], d_img.shape[3]),
         mode='bilinear', align_corners=True)
@@ -99,14 +95,13 @@ def single_test_net(net, device, img_dict, model_name, save_weights,
             weights[j, i, :, :] = refined_weight
             weights = weights / torch.sum(weights, dim=1)
 
-
       for i in range(weights.shape[1]):
         if i == 0:
           out_img = torch.unsqueeze(weights[:, i, :, :], dim=1) * imgs[i]
         else:
           out_img += torch.unsqueeze(weights[:, i, :, :], dim=1) * imgs[i]
-      
       result = ops.to_image(out_img.squeeze())
+      
       weights_img = []
       if save_weights:
         # save weights
@@ -118,99 +113,3 @@ def single_test_net(net, device, img_dict, model_name, save_weights,
 
   logging.info('End of testing')
   return result, weights_img
-
-
-
-def get_args():
-  """ Gets command-line arguments.
-
-  Returns:
-    Return command-line arguments as a set of attributes.
-  """
-
-  parser = argparse.ArgumentParser(description='Test WB Correction.')
-
-  parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?',
-                      default=1, help='Batch size', dest='batch_size')
-
-  parser.add_argument('-nrm', '--normalization', dest='norm', type=bool,
-                      default=False,
-                      help='Apply BN in network')
-
-  parser.add_argument('-ml', '--model-location', dest='model_location',
-                      default=None)
-
-  parser.add_argument('-wbs', '--wb-settings', dest='wb_settings', nargs='+',
-                      default=['D', 'S', 'T'])
-                      # default=['D', 'S', 'T', 'F', 'C'])
-
-  parser.add_argument('-sw', '--save-weights', dest='save_weights',
-                      default=True, type=bool)
-
-  parser.add_argument('-ka', '--keep-aspect-ratio', dest='keep_aspect_ratio',
-                      default=False, type=bool,
-                      help='To keep aspect ratio before processing. Only '
-                           'works when multi-scale is off.')
-
-  parser.add_argument('-ms', '--multi-scale', dest='multi_scale',
-                      default=True, type=bool)
-
-  parser.add_argument('-pp', '--post-process', dest='post_process',
-                      default=True, type=bool)
-
-  parser.add_argument('-ted', '--testing-dir', dest='tedir',
-                      default='./data/images/',
-                      help='Testing directory')
-
-  parser.add_argument('-od', '--outdir', dest='outdir',
-                      default='./results/',
-                      help='Results directory')
-
-  parser.add_argument('-g', '--gpu', dest='gpu', default=0, type=int)
-
-  parser.add_argument('-ts', '--target-size', dest='t_size', default=384,
-                      type=int,
-                      help='Size before feeding images to the network. '
-                           'Typically, 128 or 256 give good results. If '
-                           'multi-scale is used, then 384 is recommended.')
-
-  parser.add_argument('-mn', '--model-name', dest='model_name', type=str,
-                      default='WB_model_p_64_D_S_T',
-                      #default='WB_model_p_64_D_S_T',
-                      help='Model name')
-
-  return parser.parse_args()
-
-if __name__ == '__main__':
-  logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-  logging.info('Testing Mixed-Ill WB correction')
-  args = get_args()
-  
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  if device.type != 'cpu':
-    torch.cuda.set_device(args.gpu)
-
-
-  logging.info(f'Using device {device}')
-
-  net = wb_net.WBnet(device=device, norm=args.norm, inchnls=3 * len(
-    args.wb_settings))
-
-  model_path = os.path.join('models', args.model_name + '.pth')
-
-  net.load_state_dict(torch.load(model_path, map_location=device))
-
-  logging.info(f'Model loaded from {model_path}')
-
-  net.to(device=device)
-
-  net.eval()
-
-  single_test_net(net=net, device=device, data_dir=args.tedir,
-           batch_size=args.batch_size, out_dir=args.outdir,
-           post_process=args.post_process,
-           keep_aspect_ratio=args.keep_aspect_ratio,
-           t_size=args.t_size,
-           multi_scale=args.multi_scale, model_name=args.model_name,
-           save_weights=args.save_weights,
-           wb_settings=args.wb_settings)
